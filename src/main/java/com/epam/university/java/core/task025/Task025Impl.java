e95868bc84013206611a1a1e44d36bfe200f2426
Vladimir Baykalov <Baikalov.v42@yandex.ru>
2020-09-22 22:26:03 +0300

    solution task25

---
+++ src/main/java/com/epam/university/java/core/task025/Task025Impl.java
@@ -0,0 +1,29 @@
+package com.epam.university.java.core.task025;
+
+public class Task025Impl implements Task025 {
+
+    @Override
+    public int getAmountOfAlteredLetters(String sourceMessage) {
+        if (sourceMessage == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if ("".equals(sourceMessage)) {
+            return 0;
+        }
+
+        final String pattern = "SOS";
+        int count = 0;
+
+        for (int i = 0; i < sourceMessage.length(); i++) {
+
+            if (sourceMessage.charAt(i) != pattern.charAt(i % pattern.length())) {
+                count++;
+            }
+        }
+        if (count > sourceMessage.length() - 2) {
+            throw new IllegalArgumentException();
+        }
+        return count;
+    }
+}
