e2e4856bb8c0344d949071ff0d935dfe861b816c
Vladimir Baykalov <Baikalov.v42@yandex.ru>
2020-09-23 17:09:13 +0300

    solution task27

---
+++ src/main/java/com/epam/university/java/core/task027/Task027Impl.java
@@ -0,0 +1,45 @@
+package com.epam.university.java.core.task027;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Task027Impl implements Task027 {
+
+    @Override
+    public Collection<Integer> extract(String sourceString) {
+
+        if (sourceString == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (sourceString.charAt(0) == '0') {
+            return Collections.emptyList();
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 1; i < sourceString.length() / 2; i++) {
+
+            String substring = sourceString.substring(0, i);
+            Integer number = Integer.parseInt(substring);
+            numbers.add(number);
+
+            while (substring.length() < sourceString.length()) {
+                number++;
+                substring = substring + number;
+                numbers.add(number);
+                if (substring.length() > sourceString.length()
+                        || !substring.equals(sourceString.substring(0, substring.length()))) {
+                    break;
+                }
+            }
+            if (substring.equals(sourceString)) {
+                return numbers;
+            }
+            numbers.clear();
+        }
+        return Collections.emptyList();
+    }
+}
